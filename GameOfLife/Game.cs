d7c11439f78171148a18995e56e9d038fb9ab16c
Julija Kovalova <j.kovalova@accenture.com>
2020-09-04 16:38:53 +0300

    Game navigation menu

--- GameOfLife/Game.cs
+++ GameOfLife/Game.cs
@@ -101,5 +101,10 @@ namespace GameOfLife
                 }
             }
         }
+
+        public void Load()
+        {
+        
+        }
     }
 }
--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -1,11 +1,62 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
+
 
 namespace GameOfLife
 {
     public class Program
     {
+        const bool ADVANCE_MANUALLY = false;
+
         static void Main(string[] args)
+        {
+            Game game = Start();
+            if (game == null)
+            {
+                Console.WriteLine("Exiting...");
+                return;
+            }
+
+            // show initial state
+            game.Print();
+
+            // advance while playing
+            while (Advance())
+            {
+                game.Step();
+                game.Print();
+            }
+        }
+
+        static Game Start()
+        {
+            Console.WriteLine("GAME OF LIFE");
+            Console.WriteLine("1. New Game");
+            Console.WriteLine("2. Load Game");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("");
+
+            while (true)
+            {
+                Console.Write("Selection: ");
+                string input = Console.ReadLine();
+                int selection = int.Parse(input);
+
+                switch (selection)
+                {
+                    case 1:
+                        return CreateGame();
+                    case 2:
+                        return LoadGame();
+                    case 0:
+                        return null;
+                    default:
+                        Console.WriteLine("Please select item from a list.");
+                        break;
+                }
+            }
+        }
+
+        static Game CreateGame()
         {
             // read input to start the game
             Console.Write("Enter the number of rows: ");
@@ -17,14 +68,29 @@ namespace GameOfLife
             // initailize game with randomly dead or alive cells
             Game game = new Game(rows, columns);
             game.Randomize();
-            game.Print();
-            
-            // advance game every 1s
-            while (true)
+            return game;
+        }
+
+        static Game LoadGame()
+        {
+            // TODO: load from file
+            Game game = new Game(0, 0);
+            game.Load();
+            return game;
+        }
+
+        static bool Advance()
+        {
+            if (ADVANCE_MANUALLY)
             {
+                // TODO: implement advance on keypress
+                return false;
+            }
+            else
+            {
+                // advance every 1s
                 System.Threading.Thread.Sleep(1000);
-                game.Step();
-                game.Print();
+                return true;
             }
         }
     }
